092c89b9eca2148119cad73b302e583693c236fb
nikgre <niklasgreuling@t-online.de>
2019-12-07 00:35:42 +0100

    changed font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata for Powerline:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Menlo for Powerline:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
