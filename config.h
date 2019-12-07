205ea3f3fafa67f39bd5b5794c3b235938ab9247
nikgre <niklasgreuling@t-online.de>
2019-12-07 19:43:33 +0100

    added pywal colorsheme

--- config.h
+++ config.h
@@ -82,44 +82,8 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
-/* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+#include "/home/niklas/.cache/wal/colors-wal-st.h"
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-};
-
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
@@ -149,6 +113,22 @@ static unsigned int mousebg = 0;
  * doesn't match the ones requested.
  */
 static unsigned int defaultattr = 11;
+
+
+/*
+    * Xresources preferences to load at startup
+     */
+ResourcePref resources[] = {
+			{ "font",         STRING,  &font },
+			{ "termname",     STRING,  &termname },
+			{ "shell",        STRING,  &shell },
+			{ "xfps",         INTEGER, &xfps },
+			{ "actionfps",    INTEGER, &actionfps },
+			{ "blinktimeout", INTEGER, &blinktimeout },
+			{ "bellvolume",   INTEGER, &bellvolume },
+			{ "cwscale",      FLOAT,   &cwscale },
+			{ "chscale",      FLOAT,   &chscale },
+};
 
 /*
  * Internal mouse shortcuts.
